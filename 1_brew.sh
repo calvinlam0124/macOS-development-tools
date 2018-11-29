1f38303bf13c26dba46df03e023aa299358c1b12
Calvin Lam <calvin.lam@WeMines-MacBook-Pro.local>
2018-11-29 17:22:54 +0800

    update homebrew issue

--- 1_brew.sh
+++ 1_brew.sh
@@ -1,3 +1,7 @@
 # install brew
 # need entery sudo password
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Homebrew Permissions Denied Issues Solution
+# sudo chown -R $(whoami) $(brew --prefix)/*
+# reference: https://gist.github.com/irazasyed/7732946
