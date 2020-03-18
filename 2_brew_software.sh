bbf3b9394dd84101577e6195378645db9594744b
Calvin Lam <calvinlam117@gmail.com>
2020-03-18 16:44:17 +0800

    Update 2_brew_software.sh
    chore: tidy up readme

--- 2_brew_software.sh
+++ 2_brew_software.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+### Basic
+# basic tools
 brew install wget
 brew install tree
 brew install screen
@@ -9,42 +11,12 @@ brew install telnet
 # cUrl with HTTP/2
 brew install curl --with-nghttp2
 
-# Zeit/Now
-brew cask install now
-
-# ffmpeg
-brew install ffmpeg --with-fdk-aac --with-sdl2 --with-freetype --with-libass --with-libvorbis --with-libvpx --with-opus --with-x265
-
-# Pandoc and PDF library
-# need enter your password
-brew cask install basictex
-brew install pdflatex
-brew install pandoc
-
-# Play audio cli
-brew install espeak
-
-# jq for JSON pretty print
-brew install jq
-#brew install python-yq
-brew tap blacksmoke16/tap
-brew install oq
-
-# SQLite browser
-brew cask install db-browser-for-sqlite
-
-# aws CLI
-brew install awscli
-
 # Gun Core Utils - watch
 brew install coreutils 
 
 # support NTFS 
 # https://github.com/osxfuse/osxfuse/wiki/NTFS-3G
 brew install ntfs-3g
-
-# manual alternative
-brew install tldr
 
 # Create a new folder for mount ntfs
 # sudo mkdir /Volumes/NTFS
@@ -54,13 +26,69 @@ brew install tldr
 # sudo mv "/Volumes/Macintosh HD/sbin/mount_ntfs" "/Volumes/Macintosh HD/sbin/mount_ntfs.orig"
 # sudo ln -s /usr/local/sbin/mount_ntfs "/Volumes/Macintosh HD/sbin/mount_ntfs"
 
+# manual alternative
+brew install tldr
 
 # KeePass XC
 brew cask install keepassxc 
 
 
+### Server / deployment / network operation
+# aws CLI
+brew install awscli
+
+# Zeit/Now
+brew cask install now
+
+# Ngrok
+brew cask install ngrok
+
+# Ansible, Terraform
+brew install ansible
+brew install terraform
+
+# WireShark [password required]
+brew cask install wireshark
+
+# Clipboard manager
+brew cask install clipy
+
+
+### Office operation
+# Pandoc and PDF library
+# may need enter your password
+brew cask install basictex
+brew install pdflatex
+brew install pandoc
+
 # Libre Office
 brew cask install libreoffice
+
+# GIMP
+brew cask install gimp
+
+# IM - Skype, Slack
+brew cask install skype
+brew cask install slack 
+
+### Development
+# jq for JSON pretty print
+brew install jq
+#brew install python-yq
+brew tap blacksmoke16/tap
+brew install oq
+
+# SQLite browser
+brew cask install db-browser-for-sqlite
+
+# MySQL workbench
+brew cask install mysqlworkbench
+
+# DB browser for SQLite
+brew cask install db-browser-for-sqlite
+# SequelPro, Sequal-Pro-Nightly for MySQL
+brew cask install sequel-pro
+#brew cask install sequel-pro-nightly # download manaully
 
 # Atom
 brew cask install atom
@@ -71,75 +99,42 @@ brew cask install postman
 # GraphQL playground
 brew cask install graphql-playground
 
-# GIMP
-brew cask install gimp
-
 # XAMPP
 brew cask install xampp
-
-# MySQL workbench
-brew cask install mysqlworkbench
-
-# Ngrok
-brew cask install ngrok
-
-# eSpack
-brew install espeak
-
-# Ansible, Terraform
-brew install ansible
-brew install terraform
-
 
 # NodeJS
 brew install node
 
-
 # PHP, composer
 #brew tap homebrew/php
 brew install composer
+
 # for Now.sh 
 # https://github.com/juicyfx/now-php/blob/master/errors/now-dev-no-local-php.md
 brew install php
 
+# go lang
+brew install go
 
 # SourceTree
 brew cask install sourcetree
 
-
-# WireShark [password required]
-brew cask install wireshark
-
-
-# IM - Skype, Slack
-brew cask install skype
-brew cask install slack 
-
-
-# DB browser for SQLite, SequelPro, Sequal-Pro-Nightly
-brew cask install db-browser-for-sqlite
-brew cask install sequel-pro
-#brew cask install sequel-pro-nightly # download manaully
-
-
-
 # Zeplin.io
 brew cask install zeplin
-
-# Clipboard manager
-brew cask install clipy
 
 # GraphViz
 brew install graphviz
 
-# Image optimizer
-# ./guetzli --nomemlimit bees.png bees2.png
-brew install guetzli
-
-
 # git-flow
 brew install git-flow
 
+### Multimedia
+# ffmpeg
+brew install ffmpeg --with-fdk-aac --with-sdl2 --with-freetype --with-libass --with-libvorbis --with-libvpx --with-opus --with-x265
 
-# go lang
-brew install go
+# eSpack - Play audio at cli
+brew install espeak
+
+# Image optimizer
+# ./guetzli --nomemlimit bees.png bees2.png
+brew install guetzli
