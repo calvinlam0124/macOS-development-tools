eed694b4a91a549bc9d16d064a3160d2f1be43be
Calvin Lam <calvinlam117@gmail.com>
2021-01-18 12:16:28 +0800

    Update 7_stone.sh

--- 7_stone.sh
+++ 7_stone.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-dd if=/dev/urandom of=${HOME}/spare.stone bs=10240m count=1
+# 1 KB
+#dd if=/dev/urandom of=${HOME}/spare.stone bs=1024 count=1
+
+# 1 MB
+#dd if=/dev/urandom of=${HOME}/spare.stone bs=1048576 count=1
+
+# 1 GB
+#dd if=/dev/urandom of=${HOME}/spare.stone bs=1073741824 count=1
+
+# 10 GB
+dd if=/dev/urandom of=${HOME}/spare.stone bs=10737418240 count=1
